78282545b8b30dac973e0c9cb22b2704e124a6b8
vaceslavkusenko <SlavaKushch@yandex.ru>
2025-03-07 22:08:30 +0300

    Добавил исключения и проверки для из выброса

--- backend/src/main/kotlin/ru/example/demo/exception/GlobalExceptionHandler.kt
+++ backend/src/main/kotlin/ru/example/demo/exception/GlobalExceptionHandler.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import ru.example.demo.dto.response.ErrorResponse
 import ru.example.demo.exception.type.BadRequestException
+import ru.example.demo.exception.type.EntityAlreadyExistsException
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
@@ -15,10 +16,9 @@ class GlobalExceptionHandler {
     fun handleIllegalArgumentException(exception: BadRequestException) =
         ResponseEntity.badRequest().body(ErrorResponse(exception.message ?: "BAD REQUEST"))
 
-    @ExceptionHandler(EntityNotFoundException::class)
-    fun handleEntityNotFoundException(ex: EntityNotFoundException): ResponseEntity<Map<String, String>> {
-        return ResponseEntity(mapOf("error" to ex.message.orEmpty()), HttpStatus.NOT_FOUND)
-    }
+    @ExceptionHandler(EntityAlreadyExistsException::class)
+    fun handleEntityAlreadyExistsException(exception: EntityAlreadyExistsException) =
+        ResponseEntity.status(409).body(ErrorResponse(exception.message ?: "CONFLICT"))
 
     @ExceptionHandler(Exception::class)
     fun handleGeneralException(ex: Exception): ResponseEntity<Map<String, String>> {
--- backend/src/main/kotlin/ru/example/demo/service/UserService.kt
+++ backend/src/main/kotlin/ru/example/demo/service/UserService.kt
@@ -1,23 +1,35 @@
 package ru.example.demo.service
 
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import ru.example.demo.dto.enums.UserRoles
 import ru.example.demo.dto.request.RegisterHeadRequest
 import ru.example.demo.dto.response.RegisterHeadResponse
 import ru.example.demo.entity.UserCompanyEntity
 import ru.example.demo.entity.UserEntity
+import ru.example.demo.exception.type.EntityAlreadyExistsException
 import ru.example.demo.repository.UserCompanyRepository
 import java.util.*
 import kotlin.jvm.optionals.getOrElse
+
 
 @Service
 class UserService (
     val userRepository: ru.example.demo.repository.UserRepository,
     val userCompanyRepository: UserCompanyRepository
 ) {
+    @Transactional
     fun registerHead(request: RegisterHeadRequest): RegisterHeadResponse {
 
-//        val existUserCompany = userCompanyRepository.fin
+        val existUserCompany = userCompanyRepository.findByEmail(request.email)
+        if (existUserCompany != null) {
+            throw EntityAlreadyExistsException("Компания с ${request.email} уже существует")
+        }
+
+        val existUser = userRepository.findByLogin(request.headLogin)
+        if (existUser != null) {
+            throw EntityAlreadyExistsException("Пользователь с ${request.headName} уже существует")
+        }
 
         val token = generateToken()
 
